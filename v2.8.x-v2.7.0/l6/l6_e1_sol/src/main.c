a708384350f10c65a0d58f1bb6a60dd7ae842a5b
Lukasz Bielak <lukasz.bielak@nordicsemi.no>
2025-01-16 10:13:01 +0100

    Rework example

--- v2.8.x-v2.7.0/l6/l6_e1_sol/src/main.c
+++ v2.8.x-v2.7.0/l6/l6_e1_sol/src/main.c
@@ -13,65 +13,131 @@
 #include <zephyr/sys/printk.h>
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 1000
+#define DELAY_PARAM		50
 
-/* STEP 8 - Define the I2C slave device address and the addresses of relevant registers */
-#define STTS751_TEMP_HIGH_REG 0x00
-#define STTS751_TEMP_LOW_REG  0x02
-#define STTS751_CONFIG_REG    0x03
+#define BME_280_CHIP_ID		  0xD0
+
+#define CTRLHUM 		0xF2
+#define CTRLMEAS		0xF4
+#define CALIB00			0x88
+#define CALIB26			0xE1
+#define ID				0xD0
+#define PRESSMSB		0xF7
+#define PRESSLSB		0xF8
+#define PRESSXLSB		0xF9
+#define TEMPMSB			0xFA
+#define TEMPLSB			0xFB
+#define TEMPXLSB		0xFC
+#define HUMMSB			0xFD
+#define HUMLSB			0xFE
+#define DUMMY			0xFF
 
 /* STEP 6 - Get the node identifier of the sensor */
 #define I2C_NODE DT_NODELABEL(mysensor)
 
+
+/* Data structure to store BME280 data */
+struct bme280_data {
+	/* Compensation parameters */
+	uint16_t dig_t1;
+	int16_t dig_t2;
+	int16_t dig_t3;
+
+	/* Compensated values */
+	int32_t comp_temp;
+
+	/* Carryover between temperature and pressure/humidity compensation */
+	int32_t t_fine;
+
+	uint8_t chip_id;
+} bmedata;
+
+
+void bme_calibrationdata(const struct i2c_dt_spec *spec)
+{
+	uint8_t values[3];
+	uint8_t size = 3;	
+	uint8_t regaddr;
+
+	regaddr = CALIB00;
+	i2c_write_read_dt(spec,&regaddr, 1,values, 2);
+	bmedata.dig_t1 = ((uint16_t)values[1])<<8 | values[0];
+	k_msleep(DELAY_PARAM);
+
+	regaddr += 2;
+	i2c_write_read_dt(spec,&regaddr, 1,values, 2);
+	bmedata.dig_t2 = ((uint16_t)values[1])<<8 | values[1];
+	k_msleep(DELAY_PARAM);
+	
+	regaddr += 2;
+	i2c_write_read_dt(spec,&regaddr, 1,values, 2);
+	bmedata.dig_t3 = ((uint16_t)values[1])<<8 | values[1];
+	k_msleep(DELAY_PARAM);
+
+}
+static int32_t bme280_compensate_temp(struct bme280_data *data, int32_t adc_temp)
+{
+	int32_t var1, var2;
+
+	var1 = (((adc_temp >> 3) - ((int32_t)data->dig_t1 << 1)) *
+		((int32_t)data->dig_t2)) >> 11;
+	var2 = (((((adc_temp >> 4) - ((int32_t)data->dig_t1)) *
+		  ((adc_temp >> 4) - ((int32_t)data->dig_t1))) >> 12) *
+		((int32_t)data->dig_t3)) >> 14;
+
+	data->t_fine = var1 + var2;
+	return (data->t_fine * 5 + 128) >> 8;
+}
+
+
 int main(void)
 {
-
-	int ret;
 
 	/* STEP 7 - Retrieve the API-specific device structure and make sure that the device is
 	 * ready to use  */
 	static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
+
 	if (!device_is_ready(dev_i2c.bus)) {
 		printk("I2C bus %s is not ready!\n", dev_i2c.bus->name);
 		return -1;
 	}
 
-	/* STEP 9 - Setup the sensor by writing the value 0x8C to the Configuration register */
-	uint8_t config[2] = {STTS751_CONFIG_REG, 0x8C};
-	ret = i2c_write_dt(&dev_i2c, config, sizeof(config));
-	if (ret != 0) {
-		printk("Failed to write to I2C device address %x at Reg. %x \n", dev_i2c.addr,
-		       config[0]);
-		return -1;
+
+	bme_calibrationdata(&dev_i2c);
+
+	uint8_t id = 0;
+	uint8_t regs[]= {BME_280_CHIP_ID};
+
+	i2c_write_read_dt(&dev_i2c, regs,1,&id, 1);
+
+	if( id != 0x60 )
+	{
+		printk("Invalid chip id %x \n", id);
 	}
 
+	regs[0] = CTRLHUM;
+	uint8_t tmp = 0x04;
+
+	uint8_t  reg_write[] ={CTRLHUM,0x04};
+	i2c_write_dt(&dev_i2c,reg_write,2);
+
+	reg_write[0] = CTRLMEAS;
+	reg_write[1] = 0x93;
+
+	i2c_write_dt(&dev_i2c,reg_write,2);
+
 	while (1) {
-		/* STEP 10 - Read the temperature from the sensor */
-		uint8_t temp_reading[2] = {0};
-		uint8_t sensor_regs[2] = {STTS751_TEMP_LOW_REG, STTS751_TEMP_HIGH_REG};
-		ret = i2c_write_read_dt(&dev_i2c, &sensor_regs[0], 1, &temp_reading[0], 1);
-		if (ret != 0) {
-			printk("Failed to write/read I2C device address %x at Reg. %x \n",
-			       dev_i2c.addr, sensor_regs[0]);
-		}
-		ret = i2c_write_read_dt(&dev_i2c, &sensor_regs[1], 1, &temp_reading[1], 1);
-		if (ret != 0) {
-			printk("Failed to write/read I2C device address %x at Reg. %x \n",
-			       dev_i2c.addr, sensor_regs[1]);
-		}
 
-		/* STEP 11 - Convert the two bytes to a 12-bits */
-		int temp = ((int)temp_reading[1] * 256 + ((int)temp_reading[0] & 0xF0)) / 16;
-		if (temp > 2047) {
-			temp -= 4096;
-		}
+		uint8_t temp_val[3]={0};
 
-		// Convert to engineering units
-		double cTemp = temp * 0.0625;
-		double fTemp = cTemp * 1.8 + 32;
+		i2c_burst_read_dt(&dev_i2c, TEMPMSB, temp_val, 3);
 
-		// Print reading to console
-		printk("Temperature in Celsius : %.2f C \n", cTemp);
-		printk("Temperature in Fahrenheit : %.2f F \n", fTemp);
+		int32_t adc_temp = (temp_val[0] << 12) | (temp_val[1] << 1) | ((temp_val[2] >> 4) & 0x0F);
+
+		int32_t comp_temp = bme280_compensate_temp(&bmedata, adc_temp);
+		float temperature = (float)comp_temp/100.0f;
+
+		printk("Temperature: %8.2f C \n",(double)temperature);
 		k_msleep(SLEEP_TIME_MS);
 	}
 }
