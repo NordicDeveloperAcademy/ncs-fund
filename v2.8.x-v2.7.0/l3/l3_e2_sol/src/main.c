ddb92ecb87d6a56ee3ac2483bf399d82c15d2125
Heidi Sollie <heidisollie@gmail.com>
2025-03-12 14:53:55 +0100

    Added steps in the solution, and overlay file for all supported boards

--- v2.8.x-v2.7.0/l3/l3_e2_sol/src/main.c
+++ v2.8.x-v2.7.0/l3/l3_e2_sol/src/main.c
@@ -7,13 +7,17 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+/* STEP 4 - Include the header file for the custom function */
+// include "myfunction.h"
+
+/* STEP 9.1 - Check CONFIG_MYFUNCTION before including the header file */
 #ifdef CONFIG_MYFUNCTION
 #include "myfunction.h"
 #endif
 
+/* STEP 9.2 - Check CONFIG_MYFUNCTION before calling sum() */
 int main(void)
 {
-
 	while (1) {
 #ifdef CONFIG_MYFUNCTION
 		int a = 3, b = 4;
@@ -25,3 +29,13 @@ int main(void)
 		k_msleep(1000);
 	}
 }
+
+/* STEP 5 - Change main() to run the custom function */
+/*int main(void)
+{
+	int a = 3, b = 4;
+	while(1){
+		printk("The sum of %d and %d is %d\n", a, b, sum(a,b));
+		k_msleep(1000);
+	}
+}*/
