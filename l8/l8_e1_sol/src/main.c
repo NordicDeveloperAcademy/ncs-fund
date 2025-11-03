53865135f1a752cbe4c0d49a904f9ae4d428f95c
Heidi Sollie <heidisollie@gmail.com>
2025-11-03 10:58:17 +0100

    Modified exercise

--- l8/l8_e1_sol/src/main.c
+++ l8/l8_e1_sol/src/main.c
@@ -18,34 +18,28 @@
 
 /* STEP 2 - Set the priority of the producer and consumper thread */
 #define PRODUCER_PRIORITY 5
-#define CONSUMER_PRIORITY 4
+#define CONSUMER_PRIORITY 5
 
 /* STEP 9 - Define semaphore to monitor instances of available resource */
 K_SEM_DEFINE(instance_monitor_sem, 10, 10);
 
 /* STEP 3 - Initialize the available instances of this resource */
-volatile uint32_t available_instance_count = 10;
+//volatile uint32_t available_instance_count = 10;
 
 // Function for getting access of resource
 void get_access(void)
 {
-	/* STEP 10.1 - Get semaphore before access to the resource */
+	/* STEP 10.1 - Get semaphore when getting access to resource */
 	k_sem_take(&instance_monitor_sem, K_FOREVER);
-
-	/* STEP 6.1 - Decrement available resource */
-	available_instance_count--;
-	printk("Resource taken and available_instance_count = %d\n", available_instance_count);
+	printk("Resource taken and available_instance_count = %d\n", k_sem_count_get(&instance_monitor_sem));
 }
 
 // Function for releasing access of resource
 void release_access(void)
 {
-	/* STEP 6.2 - Increment available resource */
-	available_instance_count++;
-	printk("Resource given and available_instance_count = %d\n", available_instance_count);
-
-	/* STEP 10.2 - Give semaphore after finishing access to resource */
+	/* STEP 10.2 - Give semaphore when releasing access to resource */
 	k_sem_give(&instance_monitor_sem);
+	printk("Resource given and available_instance_count = %d\n", k_sem_count_get(&instance_monitor_sem));
 }
 
 /* STEP 4 - Producer thread relinquishing access to instance */
@@ -54,8 +48,7 @@ void producer(void)
 	printk("Producer thread started\n");
 	while (1) {
 		release_access();
-		// Assume the resource instance access is released at this point
-		k_msleep(500 + sys_rand32_get() % 10);
+		k_msleep(sys_rand32_get() % 10);
 	}
 }
 
@@ -65,7 +58,6 @@ void consumer(void)
 	printk("Consumer thread started\n");
 	while (1) {
 		get_access();
-		// Assume the resource instance access is released at this point
 		k_msleep(sys_rand32_get() % 10);
 	}
 }
