9b30390a5fb6c9d928786827c1cc512996fc8945
Heidi Sollie <heidisollie@gmail.com>
2025-11-03 11:00:54 +0100

    Modified exercise

--- l8/l8_e1/src/main.c
+++ l8/l8_e1/src/main.c
@@ -7,7 +7,11 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include "ncs_version.h"
+#if NCS_VERSION_NUMBER < 0x20600
+#include <zephyr/random/rand32.h>
+#else 
 #include <zephyr/random/random.h>
+#endif
 #include <string.h>
 
 #define PRODUCER_STACKSIZE 1024
@@ -15,31 +19,24 @@
 
 /* STEP 2 - Set the priority of the producer and consumer thread */
 
-
 /* STEP 9 - Define semaphore to monitor instances of available resource */
 
-
 /* STEP 3 - Initialize the available instances of this resource */
-
 
 // Function for getting access of resource
 void get_access(void)
 {
-	/* STEP 10.1 - Get semaphore before access to the resource */
-	
+	/* STEP 10.1 - Take semaphore when getting access to the resource */
 
-	/* STEP 6.1 - Decrement available resource */
-	
+	/* STEP 6.1 - Decrement available resource */	
 }
 
 // Function for releasing access of resource
 void release_access(void)
 {
-	/* STEP 10.2 - Give semaphore after finishing access to resource */
-	
+	/* STEP 10.2 - Give semaphore when releasing access to resource */
 
-	/* STEP 6.2 - Increment available resource */
-	
+	/* STEP 6.2 - Increment available resource */	
 }
 
 /* STEP 4 - Producer thread relinquishing access to instance */
